46610f0d75552b69cfda6af8eb62c2d086b99abd
Yaroslav Nosenko <yaroslav.nosenko.mail@gmail.com>
2023-05-25 13:48:05 -0400

    favicons

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,23 @@
 import './globals.css'
+import { Metadata } from 'next'
 import { Outfit } from 'next/font/google'
 import '@fontsource-variable/material-symbols-sharp'
 
 const font = Outfit({ subsets: ['latin-ext'] })
 
-export const metadata = {
-  title: 'Solidum DApp',
+export const metadata: Metadata = {
+  title: 'Solidum Assets | Create Assets from Anything',
+  icons: {
+    icon: [{ url: '/favicon.svg', type: 'image/svg+xml' }],
+    other: [
+      {
+        url: '/mask-icon.svg',
+        rel: 'mask-icon',
+        // @ts-ignore
+        color: '#000000',
+      },
+    ],
+  },
 }
 
 export default function RootLayout({
