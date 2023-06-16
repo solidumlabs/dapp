dc0da9ec83c4480e4de1e1f0109bd28914981dda
Yaroslav Nosenko <yaroslav.nosenko.mail@gmail.com>
2023-06-16 11:29:16 -0400

    updated preview

--- components/assets/PreviewInput.tsx
+++ components/assets/PreviewInput.tsx
@@ -26,7 +26,7 @@ export const PreviewInput = () => {
         <span className="absolute inset-0 w-full h-full bg-emerald-200 transition duration-300 ease-out transform -translate-x-2 -translate-y-2 group-hover:translate-x-0 group-hover:translate-y-0" />
         <span className="absolute inset-0 w-full h-full border-4 border-black"></span>
         <div className="relative">
-          <div className="relative aspect-square border-4 border-transparent">
+          <div className="relative aspect-square border-4 border-black">
             <div className="absolute w-full h-full flex-1 flex flex-col items-center justify-center">
               <i className="material-symbols-sharp text-6xl">image_search</i>
               <p className="mt-4 text-xl font-semibold text-center">
@@ -34,9 +34,9 @@ export const PreviewInput = () => {
               </p>
             </div>
             <div
-              className="absolute w-full h-full bg-contain bg-center bg-no-repeat"
+              className="absolute w-full h-full bg-contain bg-center bg-no-repeat bg-white"
               style={{
-                backgroundImage: 'url(' + url + ')',
+                backgroundImage: url ? 'url(' + url + ')' : undefined,
               }}
             />
           </div>
